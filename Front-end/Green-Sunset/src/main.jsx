a7953724afd564d1c5c300e1dc65473c119c6a88
MACF-2024 <pe.macf.1991@gmail.com>
2025-01-17 03:29:03 -0300

    Arreglo de la store

--- Front-end/Green-Sunset/src/main.jsx
+++ Front-end/Green-Sunset/src/main.jsx
@@ -3,11 +3,11 @@ import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import store from './app/store';
+import { store } from './app/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
+    <Provider store={ store }>
       <App />
     </Provider>
   </StrictMode>,
