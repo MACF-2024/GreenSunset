1117b14494c7885cf6f1c30ea909c7f231a47d0f
MACF-2024 <pe.macf.1991@gmail.com>
2025-02-03 01:39:26 -0300

    Agregar y eliminar Taste en Product (Tabla intermedia)

--- Back-end/routes/routerTaste.js
+++ Back-end/routes/routerTaste.js
@@ -4,17 +4,21 @@ const {
     tasteAll, 
     tasteById,
     tasteUpdate,
-    tasteDelete
+    tasteDelete,
+    addTasteToProduct,
+    removeTasteFromProduct
 } = require('../controllers/tasteControllers');
 
 const router = express.Router();
 
 
 router.post('/create', tasteCreate);
+router.post('/:id/product/:productId', addTasteToProduct);
 router.get('/all', tasteAll);
 router.get('/:id', tasteById);
 router.put('/update/:id', tasteUpdate);
 router.delete('/delete/:id', tasteDelete);
+router.delete('/:id/product/:productId', removeTasteFromProduct);
 
 
 module.exports = router;
