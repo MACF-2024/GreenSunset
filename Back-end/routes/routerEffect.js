ec6f8933e3a0e6691aa99ac2f9b5a67b3628f431
MACF-2024 <pe.macf.1991@gmail.com>
2025-02-03 01:58:31 -0300

    Agregar y eliminar Effect en Product (Tabla intermedia)

--- Back-end/routes/routerEffect.js
+++ Back-end/routes/routerEffect.js
@@ -4,15 +4,19 @@ const {
     effectAll, 
     effectById,
     effectUpdate,
-    effectDelete
+    effectDelete,
+    addEffectToProduct,
+    removeEffectFromProduct
 } = require('../controllers/effectControllers');
 
 const router = express.Router();
 
 router.post('/create', effectCreate);
+router.post('/:id/product/:productId', addEffectToProduct);
 router.get('/all', effectAll);
 router.get('/:id', effectById);
 router.put('/update/:id', effectUpdate);
 router.delete('/delete/:id', effectDelete);
+router.delete('/:id/product/:productId', removeEffectFromProduct);
 
 module.exports = router;
